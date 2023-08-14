62eda73f76acf45260d6a60468537698df2c354c
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-08-14 00:26:32 +0000

    Updated test files

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,7 +2,7 @@
 """
 Unittest ests for the BaseModel Class
 """
-import unittest
+import unittest, datetime
 from models.base_model import BaseModel
 
 
@@ -22,17 +22,37 @@ class TestBaseModel(unittest.TestCase):
         self.assertNotEqual(bm1.id, bm2.id)
         self.assertIsInstance(bm1.id, str)
 
-# from models.base_model import BaseModel
+    def test_id_is_string(self):
+        self.assertIsInstance(self.base_model.id, str)
 
-# my_model = BaseModel()
-# my_model.name = "My First Model"
-# my_model.my_number = 89
-# print(my_model)
-# my_model.save()
-# print(my_model)
-# my_model_json = my_model.to_dict()
-# print(my_model_json)
-# print("JSON of my_model:")
-# for key in my_model_json.keys():
-#     print("\t{}: ({}) - {}".format(key,
-# type(my_model_json[key]), my_model_json[key]))
+    def test_created_at_is_datetime(self):
+        self.assertIsInstance(self.base_model.created_at, datetime.datetime)
+
+    def test_updated_at_is_datetime(self):
+        self.assertIsInstance(self.base_model.updated_at, datetime.datetime)
+
+    def test_str_representation(self):
+        expected_str = f"[{self.base_model.__class__.__name__}] ({self.base_model.id}) {self.base_model.__dict__}"
+        self.assertEqual(str(self.base_model), expected_str)
+
+    def test_save_updates_updated_at(self):
+        old_updated_at = self.base_model.updated_at
+        self.base_model.save()
+        new_updated_at = self.base_model.updated_at
+        self.assertNotEqual(old_updated_at, new_updated_at)
+
+    def test_to_dict_returns_correct_dict(self):
+        obj_dict = self.base_model.to_dict()
+        self.assertIsInstance(obj_dict, dict)
+        self.assertEqual(obj_dict['__class__'], 'BaseModel')
+        self.assertIn('id', obj_dict)
+        self.assertIn('created_at', obj_dict)
+        self.assertIn('updated_at', obj_dict)
+
+    def test_to_dict_contains_isoformat_dates(self):
+        obj_dict = self.base_model.to_dict()
+        self.assertEqual(obj_dict['created_at'], self.base_model.created_at.isoformat())
+        self.assertEqual(obj_dict['updated_at'], self.base_model.updated_at.isoformat())
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""
+Unittest tests for the FileStorage Class
+"""
+import unittest, json, models
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+
+class TestFileStorage_instant(unittest.TestCase):
+    """
+    Testing instantiation of the FileStorage class.
+    """
+
+    def test_FStorage_instant_with_no_args(self):
+        self.assertEqual(type(FileStorage()), FileStorage)
+
+    def test_FStorage_instant_with_arg(self):
+        with self.assertRaises(TypeError):
+            FileStorage(None)
+
+    def test_file_path_is_private_str(self):
+        self.assertEqual(str, type(FileStorage._FileStorage__file_path))
+
+    def test_objects_is_private_dict(self):
+        self.assertEqual(dict, type(FileStorage._FileStorage__objects))
+
+    def test_storage_initializing(self):
+        self.assertEqual(type(models.storage), FileStorage)
+
+    def test_initialization(self):
+        self.assertIsInstance(self.storage, FileStorage)
+
+    def test_new_and_all_methods(self):
+        # Create a dummy object
+        dummy_obj = {'__class__': 'BaseModel', 'id': 'test_id'}
+        self.storage.new(dummy_obj)
+        
+        # Check if the dummy object is in the storage
+        objects = self.storage.all()
+        self.assertIn('BaseModel.test_id', objects)
+
+    def test_save_and_reload(self):
+        # Create a dummy object
+        dummy_obj = {'__class__': 'BaseModel', 'id': 'test_id'}
+        self.storage.new(dummy_obj)
+        
+        # Save and reload the storage
+        self.storage.save()
+        self.storage.reload()
+        
+        # Check if the reloaded storage contains the dummy object
+        objects = self.storage.all()
+        self.assertIn('BaseModel.test_id', objects)
+
+    def test_save_and_reload_file(self):
+        # Create a dummy object
+        dummy_obj = {'__class__': 'BaseModel', 'id': 'test_id'}
+        self.storage.new(dummy_obj)
+        
+        # Save and reload the storage
+        self.storage.save()
+        self.storage.reload()
+        
+        # Check if the content of the reloaded file matches the dummy object
+        with open(self.temp_file_path, 'r') as file:
+            data = json.load(file)
+            self.assertIn('BaseModel.test_id', data)
+
+
+if __name__ == '__main__':
+    unittest.main()
