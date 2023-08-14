fa4664fda4c3db85c22be1b4f1fc484f9c4572e5
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-08-14 00:05:19 +0000

    Updated console.py

--- console.py
+++ console.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 
+"""
 This is the console or commandline interpreter's
 definition file
 """
@@ -7,48 +7,49 @@ import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    """ 
+    """
     This is HBNBCommand class of the console
-    for the airbnb_clone project 
+    for the airbnb_clone project
     """
 
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        """ 
-        This is the quit method of the 
+        """
+        This is the quit method of the
         HBNBCommand class
         """
         return True
 
     def do_EOF(self, arg):
-        """ 
-        This is the EOF method of the 
+        """
+        This is the EOF method of the
         HBNBCommand class
         """
         print()
         return True
 
     # def do_help(self, arg):
-    #     """ 
-    #     This is the help method of the 
+    #     """
+    #     This is the help method of the
     #     HBNBCommand class
     #     """
     #     print("This is the help documentation")
 
     def help_quit(self):
-        """ 
-        This is the help_quit method of the 
+        """
+        This is the help_quit method of the
         HBNBCommand class
         """
         print("Quit command to exit the program")
 
     def empty_line(self):
-        """ 
-        This is the empty line method of the 
+        """
+        This is the empty line method of the
         HBNBCommand class. Empty line + ENTER shouldn't execute anything
         """
         pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
