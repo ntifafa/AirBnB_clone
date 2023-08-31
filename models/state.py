0cc745084316d76d1194b0183c9fe86e59ac7474
reginaldbdanso <reginaldbdanso@gmail.com>
2023-08-31 14:44:22 +0000

    Added state class

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module is about a class State
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """State class that inherits from BaseModel"""
+    name = ""
