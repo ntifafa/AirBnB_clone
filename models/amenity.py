44e94dbbc59b6feda02421373d6f2f6f6d0038b4
reginaldbdanso <reginaldbdanso@gmail.com>
2023-08-31 14:43:43 +0000

    Added amenity

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This module is about a class Amenity
+"""
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """Amenity class that inherits from BaseModel"""
+    name = ""
