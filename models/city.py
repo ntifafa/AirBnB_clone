24247a9fc177897f6c3992816ef3e31cee7cc98a
reginaldbdanso <reginaldbdanso@gmail.com>
2023-08-31 14:43:58 +0000

    Added city class

---
+++ models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module is about a class City
+"""
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """City class that inherits from BaseModel"""
+    state_id = ""
+    name = ""
+
